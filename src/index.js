08bf824c0dc8b3f241e88326bbef297fed2d8136
harttle <yangjvn@126.com>
2020-09-08 16:06:00 +0800

    chore: webpack build

---
+++ src/index.js
@@ -0,0 +1,5 @@
+import App from './app.san'
+
+const el = document.querySelector('#app')
+const app = new App();
+app.attach(el);
---
+++ webpack.config.js
@@ -0,0 +1,38 @@
+const SanLoaderPlugin = require('san-loader/lib/plugin');
+const HtmlWebpackPlugin = require('html-webpack-plugin');
+const path = require('path');
+
+module.exports = {
+    entry: './src/index.js',
+    output: {
+        path: path.resolve(__dirname, 'dist'),
+        filename: 'index_bundle.js'
+    },
+    mode: 'development',
+    module: {
+        rules: [
+            {
+                test: /\.san$/,
+                loader: 'san-loader'
+            },
+            {
+                test: /\.js$/,
+                loader: 'babel-loader'
+            },
+            {
+                test: /\.html$/,
+                loader: 'html-loader'
+            },
+            {
+                test: /\.less$/,
+                use: ['style-loader', 'css-loader', 'less-loader']
+            }
+        ]
+    },
+    plugins: [
+        new SanLoaderPlugin(),
+        new HtmlWebpackPlugin({
+            template: 'src/index.html'
+        })
+    ]
+};
